5a67fcef95d6d3d31750a53320db9f2b0d7d45be
Vinicius Jesus <viniciusantos2105@gmail.com>
2022-08-01 11:57:45 -0300

    update config security

--- src/main/java/com/security/security/config/SecurityConfig.java
+++ src/main/java/com/security/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.security.security.config;
 
+import com.security.security.security.JwtAuthFilter;
+import com.security.security.security.JwtService;
 import com.security.security.service.ClientServicImpl;
 import jdk.jfr.Enabled;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +11,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,9 +23,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ClientServicImpl clientService;
 
+    @Autowired
+    private JwtService jwtService;
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, clientService);
     }
 
     @Override
@@ -35,6 +48,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     permitAll()
                 .antMatchers("/user/findAll")
                     .hasRole("ADMIN")
-                .and().httpBasic();
+                .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
